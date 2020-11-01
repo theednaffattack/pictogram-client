ce75e0fd337e6b7e4a85bb585513d78a3984a896
Eddie Naff <eddie@eddienaff.dev>
2020-11-01 10:25:40 -0800

    Remove logs

--- src/lib/utilities.redirect.ts
+++ src/lib/utilities.redirect.ts
@@ -1,15 +1,24 @@
 import Router from "next/router";
 import { MyContext } from "./types";
+import { isServer } from "./utilities.is-server";
 
 function redirect(context: MyContext, target: string) {
   if (context.res) {
+    console.log("VIEW CONTEXT", context);
+
     // server
     // 303: "See other"
     context.res.writeHead(303, { Location: target });
     context.res.end();
+  }
+  if (!isServer()) {
+    {
+      // In the browser, we just pretend like this never even happened ;)
+      Router.replace(target);
+    }
   } else {
-    // In the browser, we just pretend like this never even happened ;)
-    Router.replace(target);
+    //
+    console.log("ERROR UNEXPECTED STATE DURING RE-ROUTE", context);
   }
 }
 
